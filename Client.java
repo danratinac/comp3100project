6a9d2784897dd68d90b2ab07b9fc07a3061e8f38
Dan [home PC] <danratinac@gmail.com>
2022-05-16 22:20:56 +1000

    first iteration of custom scheduling algorithm

--- Client.java
+++ Client.java
@@ -1,9 +1,13 @@
+// avg turnaround time for ds-client (using week 9 config):
+// fc: 1805
+// ff: 1405
+// wf: 1802
+// bf: 1405
+
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.plaf.synth.SynthSeparatorUI;
 
 import Utilities.JobInfo;
 import Utilities.ServerInfo;
@@ -18,6 +22,7 @@ public class Client {
                                                                                   // largest type
 
     private static final int DEFAULT_PORT = 50000;
+    private static final int MAX_RUNTIME = 1500;
 
     public static void main(String[] args) {
         try {
@@ -76,8 +81,7 @@ public class Client {
             // store server info
             for (int i = 0; i < servers.length; i++) {
                 msg = in.readLine();
-                info = msg.split(" ");
-                servers[i] = new ServerInfo(info[0], info[1], info[4], info[5], info[6], info[7], info[8]);
+                servers[i] = extractServerInfo(msg);
             }
 
             // print server info
@@ -140,11 +144,13 @@ public class Client {
                         currentJob = extractJobInfo(msg);
 
                         switch (args[0]) {
-                            case "fc":
+                            case "-fc":
                                 scheduleJobFc(in, out);
                                 break;
-                            default:
+                            case "-lrr":
                                 scheduleJobLrr(out);
+                            default:
+                                scheduleJobCustom(in, out);
                                 break;
                         }
 
@@ -193,6 +199,13 @@ public class Client {
         String[] info = msg.split(" ");
         JobInfo job = new JobInfo(info[1], info[2], info[3], info[4], info[5], info[6]);
         return job;
+    }
+
+    // extracts server info in useable format from a ds-server GETS record
+    private static ServerInfo extractServerInfo(String msg) {
+        String[] info = msg.split(" ");
+        ServerInfo server = new ServerInfo(info[0], info[1], info[4], info[5], info[6], info[7], info[8]);
+        return server;
     }
 
     // returns a list of all servers that are of the type with most CPU cores
@@ -266,6 +279,93 @@ public class Client {
             out.flush();
 
             System.out.println("Sent: " + msg);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void scheduleJobCustom(BufferedReader in, DataOutputStream out) {
+        try {
+            // check for servers with necessary resources currently available
+            String msg = "GETS Avail " + currentJob.reqCores + " " + currentJob.reqMem + " "
+                    + currentJob.reqDisk;
+            out.write((msg + "\n").getBytes());
+            out.flush();
+
+            System.out.println("Sent: " + msg);
+
+            // get data and discard, we don't need any of the info it contains
+            System.out.println("Recieved: " + in.readLine());
+            out.write(("OK\n").getBytes());
+            out.flush();
+            System.out.println("Sent: OK");
+
+            // get info on first available server
+            String rply = in.readLine();
+
+            // if there are servers with the required resources available, schedule to the first one
+            if (!rply.equals(".")) {
+                // get first available server
+                ServerInfo server = extractServerInfo(rply);
+
+                // send scheduling request
+                msg = "SCHD " + currentJob.id + " " + server.type + " " + server.id;
+                out.write((msg + "\n").getBytes());
+                out.flush();
+
+                System.out.println("Sent: " + msg);
+            } else { // otherwise fall back to the servers that can eventually provide the required resources
+                // get capable servers
+                msg = "GETS Capable " + currentJob.reqCores + " " + currentJob.reqMem + " "
+                    + currentJob.reqDisk;
+                out.write((msg + "\n").getBytes());
+                out.flush();
+
+                System.out.println("Sent: " + msg);
+
+                // get data
+                rply = in.readLine();
+                System.out.println("Recieved: " + rply);
+
+                // send OK
+                out.write(("OK\n").getBytes());
+                out.flush();
+                System.out.println("Sent: OK");
+    
+                ServerInfo[] capServers = new ServerInfo[Integer.valueOf(rply.split(" ")[1])];
+
+                // get server info
+                for (int i = 0; i < capServers.length; i++) {
+                   capServers[i] = extractServerInfo(in.readLine());
+                }
+
+                // find the first capable server with an estimated runtime under the threshold
+                int index = 0;
+                int currentEstRuntime = 0;
+                do {
+                    // get total estimate runtime for server
+                    msg = "EJWT " + capServers[index].type + " " + capServers[index].id;
+                    out.write((msg + "\n").getBytes());
+                    out.flush();
+
+                    System.out.println("Sent: " + msg);
+
+                    rply = in.readLine();
+                    System.out.println("Received: " + rply);
+
+                    currentEstRuntime = Integer.valueOf(rply);
+
+                    index++;
+                } while (currentEstRuntime < MAX_RUNTIME);
+
+                // send scheduling request
+                msg = "SCHD " + currentJob.id + " " + capServers[index].type + " " + capServers[index].id;
+                out.write((msg + "\n").getBytes());
+                out.flush();
+
+                System.out.println("Sent: " + msg);
+            }
+
         } catch (Exception e) {
             System.out.println(e);
         }
