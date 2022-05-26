fca8c4f4d0bf8db2b7d667db25388ad591016207
Dan [home PC] <danratinac@gmail.com>
2022-05-26 22:26:24 +1000

    increased MAX_RUNTIME slightly

--- Client.java
+++ Client.java
@@ -22,7 +22,7 @@ public class Client {
                                                                                   // largest type
 
     private static final int DEFAULT_PORT = 50000;
-    private static final int MAX_RUNTIME = 200;
+    private static final int MAX_RUNTIME = 300;
 
     public static void main(String[] args) {
         try {
